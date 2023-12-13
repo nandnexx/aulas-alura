734832212ae0133b8d6b6528a303db542821bb94
Fernando <nando14-83@hotmail.com>
2023-12-13 12:51:21 -0300

    codigos var e conversoes

---
+++ conversoes.js
@@ -0,0 +1,12 @@
+// tipo de dado
+// booleanos
+
+// conversão implicita
+const numero = 456;
+const numeroString = Number("456a");
+
+// Number() transforma string em numero
+// String() transforma numero em string(texto)
+console.log(numero + numeroString)
+
+// conversão explícita 
---
+++ type-boolean.js
@@ -0,0 +1,14 @@
+// boolean
+// true -> verdadeiro
+// false -> falso
+
+const primeiroNum = 5;
+const segundoNum = 5;
+const cadastroAtivado = true;
+
+// console.log(primeiroNum === segundoNum);
+
+const texto1 = "Alura";
+const texto2 = "alura";
+
+console.log(texto1 === texto2)
